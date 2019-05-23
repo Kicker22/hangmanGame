e2509fd4a46591e3b59b31e3ff4ac18db90ad234
Matt <mattsully01@gmail.com>
2019-05-23 11:01:37 -0600

    created html structure  and spaced elements on page

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+
+// database of random words
+
+// chooses a random word from data base
+
+// listens for user keyup 
+
+// compares user input to chosen word and checks if correct
+
+// keeps track of user hits and misses
+
+// displays image based on word guessed 
