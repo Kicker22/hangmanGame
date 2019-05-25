a8aa4b685743f12544cd57331500d96adf6c0bb8
Matt <mattsully01@gmail.com>
2019-05-25 13:11:59 -0600

    commiting javascript functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,32 @@
 
 // database of random words
+var wordDataBase = ["retro", "delorean", "kungfurry", "neon" ]
+
 
 // chooses a random word from data base
+function currentWord(){
+    // loop my database 
+    for(i = 0; i < wordDataBase.length; i++){
+        console.log(wordDataBase[i]);
+        //changes DOM for specified id
+        var newWord = wordDataBase[Math.floor(Math.random() * wordDataBase.length)];
+        document.getElementById("word").innerHTML = newWord;
+    }
+}
 
+// function that shows letters instread of dashes
+function showDashes(){
+    let displayWord = '';
+    for(d = 0; d < wordDataBase.length; i++){
+        displayWord = displayWord + '_' +' '
+        document.getElementById("word").textContent = displayWord;
+    }
+}
+
+//calls function
+
+currentWord()
+showDashes()
 // listens for user keyup 
 
 // compares user input to chosen word and checks if correct
