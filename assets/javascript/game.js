0a34449af8f2a3058f660fc96478ff5244599198
Matt <mattsully01@gmail.com>
2019-05-25 19:39:35 -0600

    was able to set string to underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,22 @@
+const possibleWords = ["cat", "dog", "lizard", "bear"];
+var random = Math.floor(Math.random() * possibleWords.length);
+let chosenWord = possibleWords[random];
+var underScore = [];
+console.log(chosenWord)
 
-// database of random words
-var wordDataBase = ["retro", "delorean", "kungfurry", "neon" ]
 
-
-// chooses a random word from data base
-function currentWord(){
-    // loop my database 
-    for(i = 0; i < wordDataBase.length; i++){
-        console.log(wordDataBase[i]);
-        //changes DOM for specified id
-        var newWord = wordDataBase[Math.floor(Math.random() * wordDataBase.length)];
-        document.getElementById("word").innerHTML = newWord;
-    }
+function initGame(){
+    setUnderScore()
 }
 
-// function that shows letters instread of dashes
-function showDashes(){
-    let displayWord = '';
-    for(d = 0; d < wordDataBase.length; i++){
-        displayWord = displayWord + '_' +' '
-        document.getElementById("word").textContent = displayWord;
+initGame()
+
+//create underscores based on word length
+function setUnderScore(){
+    for(let i = 0; i < chosenWord.length; i++){
+        underScore.push(' _ ' +' ' + ' ')
+        console.log(chosenWord)
+        document.getElementById("word").innerText = underScore;
+        console.log(underScore)
     }
 }
-
-//calls function
-
-currentWord()
-showDashes()
-// listens for user keyup 
-
-// compares user input to chosen word and checks if correct
-
-// keeps track of user hits and misses
-
-// displays image based on word guessed 
