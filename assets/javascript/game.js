1e000bf29ebfa9b42cbab048df749d174450b6ad
Matt <mattsully01@gmail.com>
2019-05-27 12:42:16 -0600

    removed css to trouble shoot html elements not responding also added more to event listeners

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,80 @@
-const possibleWords = ["retro", "delorean", "neon", "dystopia"];
+let possibleWords = ["retro", "delorean", "neon", "dystopia"];
 var random = Math.floor(Math.random() * possibleWords.length);
-let chosenWord = possibleWords[random];
-var underScore = [];
+let currentWord ='neon';
+let wins = 0;
+console.log(currentWord)
 var lettersGuessed = [];
+console.log(lettersGuessed)
 
 
 function initGame(){
-    setUnderScore()
-    keyUpListener()
+   assignCurrentWord()
+   eventListener()
+   updateDOM()
 }
 
 initGame()
-
-//create underscores based on word length
-function setUnderScore(){
-    for(let i = 0; i < chosenWord.length; i++){
-        underScore.push(' _ ' +' ' + ' ')
-        document.getElementById("word").innerText = underScore;
+function wordHasBeenGuessed(){
+    for(let i=0; i < currentWord.length; i++){
+        if(lettersGuessed.includes(currentWord[i])){
+    
+        } else {
+            return false;
+        }
+        return true;
     }
 }
+        //initial code i made up thats sorta worked. but staticaly placed _ for every letter 
+// //create underscores based on word length
+// function setUnderScore(){
+//     for(let i = 0; i < chosenWord.length; i++){
+//         underScore.push(' _' + '  ' + ' ')
+//         document.getElementById("word").innerText = underScore;
+//     }
+// }
 
 
+// function that pulls random word from the array
+function assignCurrentWord() {
+    const index = random;
+    currentWord = possibleWords[index]
+}
 
-//create a key listener event that listens for keyup 
-function keyUpListener(){
-    var guessedWord = document.getElementById('word')
+function eventListener(){
     document.onkeyup = function(event){
         lettersGuessed.push(event.key)
-        for(i = 0; i < guessedWord.lentgh; i++){
-            if(lettersGuessed.includes(chosenWord[i])){
-                chosenWord = chosenWord + guessedWord[i] + ' '
-            }
+        if(wordHasBeenGuessed()){
+            wins++
+            document.getElementById('current-wins').text = wins
+            initGame()
         }
-        document.getElementById('word').textContent = lettersGuessed[i] + underScore;
-        console.log(lettersGuessed)
+        updateDOM()
+        console.log(wins)
     }
 }
+
+//update DOM
+function updateDOM(){;
+    showLetterOrDash()
+    document.getElementById("current-letters")
+}
+
+// display letters or dashes also checks for condition
+function showLetterOrDash(){
+    let displayWord = ' '
+    for(let i = 0; i< currentWord.length; i++){
+        if(lettersGuessed.includes(currentWord[i])){
+            displayWord = displayWord + currentWord[i] + ' '
+        } else {
+            console.log(displayWord)
+            displayWord = displayWord + '_' + ' '
+        }
+    }
+    document.getElementById('current-word').textContent = displayWord
+}
+                
+                
+
+
+
     
